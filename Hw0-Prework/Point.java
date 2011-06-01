a1e347820601e7a7f1cb9c0c5403aa6fd5e72566
gas-par <gaspar@brandeis.edu>
2011-05-31 19:00:23 -0700

    Edited Hw0-Prework/Point.java via GitHub

--- Hw0-Prework/Point.java
+++ Hw0-Prework/Point.java
@@ -1,3 +1,8 @@
+/**
+* class that creates a point struct
+* each point has x,y coordinate, a value it holds(wall or space)
+* and whether the point's been visited or not
+*/
 public class Point{
 		int x,y;
 		int value;
